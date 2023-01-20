0e2313bcec32deed9959e02e3154e87e8e04278b
wilczany <wilczewski.2407@gmail.com>
2023-01-20 06:27:27 +0100

    commit

--- hash.py
+++ hash.py
@@ -1,40 +1,85 @@
-import random
+def Eukl(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a == 1
 
 
-class hasz_mapa():
+class HaszMapa:
+    hasz = "kwadratowe"
 
     def __init__(self, size):
         self.rozmiar = size
-        self.tablica = [None for _ in range(self.rozmiar)]
+        self.tablica = [[None, -1] for _ in range(self.rozmiar)]
+        self.ilosc = 0
         pass
 
     def clear(self):
         self.tablica = [None for _ in range(self.rozmiar)]
 
-    def put(self, object, key):
+    def put(self, item, key):
         hasz = self.__haszowanie(key)
+        i = 0
         while True:
-            if self.tablica[hasz] is None:
-                self.tablica[hasz] = object
+            if self.tablica[hasz][0] is None:
+                self.tablica[hasz][0] = item
+                self.tablica[hasz][1] = key
                 break
             else:
-                # rozwiązanie problemu adresowania
-                pass
+                # jezeli taki sam klucz to zastepje obiekt
+                if self.tablica[hasz][1] == key:
+                    self.tablica[hasz][0] = object
+                    break
+                else:
+                    i += 1
+                    hasz = self.__haszowanie(key, i)
+
         return hasz
 
     def get(self, key):
         hasz = self.__haszowanie(key)
-        while
+        i = 0
+        while True:
+            if self.tablica[hasz][1] == key:
+                return self.tablica[hasz][0]
+            else:
+                i += 1
+                hasz = self.__haszowanie(key, i)
 
-        pass
+    def remove(self, key):
+        hasz = self.__haszowanie(key)
+        i = 0
+        if self.tablica[hasz][1] == key:
+            return self.tablica[hasz][0]
+        else:
+            i += 1
+            hasz = self.__haszowanie(key, i)
 
-    def remove(self):
-        pass
+    def __haszowanie(self, klucz, krotka=0) -> int:
 
-    def __haszowanie(self, klucz) -> int:
-        key = random.randint(0, self.rozmiar - 1)
+        #hasz1() = klucz mod rozmiar
+        #hasz2() = wynik funkcji ma być względnie pierwszy z rozmiarem tablicy ?
 
-        pass
+        if krotka == 0:
+            return klucz % self.rozmiar
+        coprime = krotka * (klucz %)
+        Eukl(self.rozmiar, b)
+
+        return (
+                       krotka * (klucz % self.ilosc)
+                       + klucz % self.rozmiar - 1
+               ) \
+               % self.rozmiar
 
     def show(self):
-        pass
+        i = 0
+        for h in range(self.rozmiar):
+            if i == self.ilosc:
+                break
+            if self.tablica[h][0] is not None:
+                print("[" + str(h) + "]"
+                      + "--->"
+                      + "Object: " + str(self.tablica[h][0])
+                      + "\tKey: " + str(self.tablica[h][1])
+                      )
+
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import hash as h
+
 d = {
     'Włochy',
     'Polska'
@@ -6,8 +7,9 @@ d = {
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    mapa = h.hasz_mapa(20)
-    print(mapa.tablica)
-    print(mapa.__hash__())
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # mapa = h.hasz_mapa(20)
+    #
+    # mapa.put("kupsko", 69)
+    # mapa.put("kutas", 420)
+    # mapa.put("ruchanie", 2137)
+    # mapa.put("dziwka", 111029)
